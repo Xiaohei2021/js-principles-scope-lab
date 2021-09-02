9167a54572834632bb665344c12b41ea1b5358f8
Leon <hualiang.tian@gmail.com>
2021-09-01 21:21:43 -0400

    commit tree d15cb27ef3957b69d7995903114d5573ac68a203

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "kim";
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase(); 
+    // let upperName = customerName.toUpperCase(); 
+    // return upperName;
+}
+
+function setBestCustomer(){
+   bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "kanye";
+    return leastFavoriteCustomer;
+}
